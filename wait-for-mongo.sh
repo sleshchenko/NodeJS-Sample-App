e5645f00e587a45486ccf5d2f4e4ef7a12a614b0
Sergii Leshchenko <sleshche@redhat.com>
2019-04-29 16:45:35 -0700

    Add timeout for one request to mongo while waiting

--- wait-for-mongo.sh
+++ wait-for-mongo.sh
@@ -3,7 +3,7 @@
 : ${WAIT_MONGO_HOST:=mongo}
 : ${WAIT_MONGO_PORT:=27017}
 
-until nc -z $WAIT_MONGO_HOST $WAIT_MONGO_PORT
+until nc -w 3 -z $WAIT_MONGO_HOST $WAIT_MONGO_PORT
 do
     echo "Waiting for Mongo ($WAIT_MONGO_HOST:$WAIT_MONGO_PORT) to start..."
     sleep 0.5
